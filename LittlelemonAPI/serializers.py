2c906c07544f63042294eee202efd0de4e13eab8
Yash Jogdand <yashjogdandyj@gmail.com>
2025-12-29 15:46:50 +0530

    DataCleaning-Implemeted

--- LittlelemonAPI/serializers.py
+++ LittlelemonAPI/serializers.py
@@ -2,6 +2,7 @@ from rest_framework import serializers # Import the serializers module from Djan
 from .models import MenuItem # Import the MenuItem model from the current directory
 from .models import Category # Import the Category model from the current directory
 from decimal import Decimal # Import the Decimal module for precise decimal arithmetic
+import bleach
 
 # class MenuItemSerializer(serializers.Serializer):
 #     id = serializers.IntegerField()
@@ -14,6 +15,9 @@ class CategorySerializer(serializers.ModelSerializer): # Define a serializer for
         #Not added yet (Relationship Serializer)
         model = Category # The model to be serialized
         fields = ['id', 'slug', 'title'] # The fields to be included in the serialized output
+    
+    def validate_title(self, value):
+        return bleach.clean(value, tags=[], attributes={}, strip=True)
 
 class MenuItemSerializer(serializers.ModelSerializer): # Define a serializer for the MenuItem model
     stock = serializers.IntegerField(source='inventory') # Map the 'stock' field to the 'inventory' model field
@@ -24,17 +28,20 @@ class MenuItemSerializer(serializers.ModelSerializer): # Define a serializer for
         model = MenuItem # The model to be serialized
         fields = ['id', 'title', 'price', 'stock', 'price_after_tax', 'category', 'category_id'] # The fields to be included in the serialized output
 
+    def validate_title(self, value):
+        return bleach.clean(value, tags=[], attributes={}, strip=True)
+
     def calculate_tax(self, product:MenuItem) -> Decimal: # A method to calculate the price after tax
         return product.price * Decimal(1.1) # Calculate and return the price with a 10% tax
     
-def create(self, validated_data): # A method to handle the creation of a new MenuItem
-    category_data = validated_data.pop('category', None) # Get the category data from the validated data
+    def create(self, validated_data): # A method to handle the creation of a new MenuItem
+        category_data = validated_data.pop('category', None) # Get the category data from the validated data
 
-    if category_data: # If category data is present
-        category, _ = Category.objects.get_or_create( # Get or create a Category object
-            slug=category_data['slug'], # The slug of the category
-            defaults={'title': category_data['title']} # The title of the category
-        )
-        validated_data['category'] = category # Set the category of the MenuItem
+        if category_data: # If category data is present
+            category, _ = Category.objects.get_or_create( # Get or create a Category object
+                slug=category_data['slug'], # The slug of the category
+                defaults={'title': category_data['title']} # The title of the category
+            )
+            validated_data['category'] = category # Set the category of the MenuItem
 
-    return super().create(validated_data) # Call the parent's create method to create the MenuItem
+        return super().create(validated_data) # Call the parent's create method to create the MenuItem
--- LittlelemonAPI/tests.py
+++ LittlelemonAPI/tests.py
@@ -3,7 +3,8 @@
 # Django provides a test framework that allows you to write and run tests for your app.
 
 from django.test import TestCase
-from .models import MenuItem
+from .models import MenuItem, Category
+from .serializers import MenuItemSerializer
 
 # The TestCase class is a subclass of the standard Python unittest.TestCase class.
 # It provides a number of additional assertion methods that are useful for testing web applications.
@@ -11,8 +12,9 @@ class MenuItemTest(TestCase):
     # The setUp method is called before each test method is run.
     # It is used to set up any objects that are needed for the tests.
     def setUp(self):
-        MenuItem.objects.create(title="Ice Cream", price=80, inventory=100)
-        MenuItem.objects.create(title="Cake", price=100, inventory=50)
+        self.category = Category.objects.create(slug="test-desserts", title="Desserts")
+        MenuItem.objects.create(title="Ice Cream", price=80, inventory=100, category=self.category)
+        MenuItem.objects.create(title="Cake", price=100, inventory=50, category=self.category)
 
     # Test methods must start with the word 'test'.
     def test_get_item(self):
@@ -26,5 +28,20 @@ class MenuItemTest(TestCase):
         items = MenuItem.objects.all()
         # The assertEqual() method is used to check that two values are equal.
         self.assertEqual(len(items), 2)
+    
+    def test_title_html_stripping(self):
+        """
+        Tests that HTML tags are stripped from the title field during serialization.
+        """
+        data = {
+            "title": "<a>HTML</a> Title",
+            "price": "50.00",
+            "stock": 30,
+            "category_id": self.category.id
+        }
+        serializer = MenuItemSerializer(data=data)
+        self.assertTrue(serializer.is_valid(raise_exception=True))
+        self.assertEqual(serializer.validated_data['title'], "HTML Title")
+
 
 # Create your tests here.
