78629fa1e326c120b8a4244efe49bb5ddbc1ea53
Yash Jogdand <yashjogdandyj@gmail.com>
2026-05-25 19:13:30 +0530

    feat: implement API service layer and global state contexts

---
+++ little-lemon-ui/src/services/api.ts
@@ -0,0 +1,17 @@
+import axios from 'axios';
+
+const API_URL = 'http://localhost:8000';
+
+const api = axios.create({
+  baseURL: API_URL,
+});
+
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem('token');
+  if (token) {
+    config.headers.Authorization = `Token ${token}`;
+  }
+  return config;
+});
+
+export default api;
